812dc505e8abb12e4c0a429bbbfbb0394b28695f
DavidSheltonSF <david.faria012@gmail.com>
2024-04-13 18:38:19 -0300

    refactor(Cards): Turning Cards elements more reutilizable

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,50 +1,31 @@
-import CardsItem from "./ CardsItem";
 import "./Cards.css"
+import CardsItem from "./ CardsItem"
+/*
 import img9 from "../../public/images/img-9.jpg"
 import img2 from "../../public/images/img-2.jpg"
 import img3 from "../../public/images/img-3.jpg"
 import img4 from "../../public/images/img-4.jpg"
 import img8 from "../../public/images/img-8.jpg"
+*/
 
-function Cards(){
+
+function Cards({title, cardList}){
   return <>
     <div className="cards">
-      <h1>Check out these EPIC Destinations!</h1>
+      {title ? <h1>{title}</h1> : ''}
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardsItem 
-            src={img9}
-            text="Explore the hidden waterfall deep inside the Amazon Jungle"
-            label="Adventure"
-            path="/services"
-            />
-            <CardsItem 
-            src={img2}
-            text="Travel through the Islands of Bali in a private Cruise"
-            label="Luxury"
-            path="/services"
-            />
-          </ul>
-          <ul className="cards__items">
-            <CardsItem 
-            src={img3}
-            text="Set Sail in the Atlantic Ocean Uncharted Waters"
-            label="Mistery"
-            path="/services"
-            />
-            <CardsItem 
-            src={img4}
-            text="Experience Football on Top of Mountains"
-            label="Adventure"
-            path="/services"
-            />
-            <CardsItem 
-            src={img8}
-            text="Ride through the Sahara Desert camel tour"
-            label="Adrenaline"
-            path="/services"
-            />
+           {cardList.map((element, index) => {
+             return <>
+               <CardsItem
+                src={element.src}
+                text={element.text}
+                label={element.label}
+                path={element.path}
+               />
+             </>
+           })}
           </ul>
         </div>
       </div>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,11 +2,55 @@ import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
 import Footer from '../Footer';
+import img9 from "../../../public/images/img-9.jpg"
+import img2 from "../../../public/images/img-2.jpg"
+import img3 from "../../../public/images/img-3.jpg"
+import img4 from "../../../public/images/img-4.jpg"
+import img8 from "../../../public/images/img-8.jpg"
 
 function Home() {
+
+  const cardList1 = [
+    {
+      src: img9,
+      text: "Explore the hidden waterfall deep inside the Amazon Jungle", 
+      label: "Adventure", 
+      path: "/services"
+    },
+    {
+      src: img2,
+      text: "Travel through the Islands of Bali in a private Cruise", 
+      label: "Luxury", 
+      path: "/services"
+    },
+
+  ]
+
+  const cardList2 = [
+    {
+      src: img3,
+      text: "Set Sail in the Atlantic Ocean Uncharted Waters", 
+      label: "Mystery", 
+      path: "/services"
+    },
+    {
+      src: img4,
+      text: "Experience Football on Top of Mountains", 
+      label: "Adventure", 
+      path: "/services"
+    },
+    {
+      src: img8,
+      text: "Ride through the Sahara Desert camel tour", 
+      label: "Adrenaline", 
+      path: "/services"
+    }
+  ]
+
   return <>
     <HeroSection/>
-    <Cards/>
+    <Cards title="Check out these EPIC Destinations!" cardList={cardList1}/>
+    <Cards cardList={cardList2}/>
     <Footer/>
   </>
 }
